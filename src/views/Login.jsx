f1956ea2ddcda17532c3a4f167fb0035ae8a8151
ByronEG635 <byronefrain635@gmail.com>
2025-10-17 01:11:41 -0600

    mejora de la vista de registro de usuario

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -130,8 +130,8 @@ const Login = () => {
 
             <div className="auth-footer">
               <span>¿No tienes una cuenta? </span>
-              <button type="button" className="link" onClick={() => navigate('/register')}>
-                Crear cuenta
+              <button type="button" className="link" onClick={() => navigate('/regisuser')}>
+                Registrar usuario
               </button>
             </div>
           </div>
